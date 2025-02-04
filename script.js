06f69ba8a8c26084c05d8b4409f50e57a4741963
ngvf122 <yjyj_1222@naver.com>
2025-02-05 01:26:29 +0900

    이미지 추가  기능 추가

--- script.js
+++ script.js
@@ -20,6 +20,37 @@ function change_text(e) {
 function change_size(e) {
   document.getElementById("text").style.fontSize = `${(e.value / 100) * 3.75}rem`;
 }
+function change_mode(e) {
+  if (e.checked) {
+    document.getElementById("TextArea").style.display = "none";
+    document.getElementById("ImageArea").style.display = "";
+    document.getElementById("ImgData").style.display = "";
+    document.getElementById("text").style.display = "none";
+  } else {
+    document.getElementById("TextArea").style.display = "";
+    document.getElementById("ImageArea").style.display = "none";
+    document.getElementById("ImgData").style.display = "none";
+    document.getElementById("text").style.display = "";
+  }
+}
+async function get_image_src(image) {
+  return await new Promise((resolve, reject) => {
+    reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(image);
+  });
+}
+async function change_textover_image(e) {
+  file_name = e.files[0].name;
+  document.getElementById("file_select").innerText = file_name;
+  image_src = await get_image_src(e.files[0]);
+  document.getElementById("ImgData").src = image_src;
+}
+function change_image_size(e) {
+  document.getElementById("ImgData").style.width = `${e.value}%`;
+}
 async function export_image() {
   //   canvas = await html2canvas(document.getElementById("result"));
   const a = document.createElement("a");
@@ -34,6 +65,7 @@ async function export_image() {
     a.click();
   });
 }
+
 const register = () => {
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
